38a9e3c1bb059b5e584d93560b849a15c1321b3a
Joseph Tam <josephtam3@hotmail.com>
2016-01-13 23:07:59 -0800

    work prior to tictactoe code

--- index.php
+++ index.php
@@ -6,7 +6,23 @@
     </head>
     <body>
         <?php
-            echo 'hello world';
+          $name = 'Jim';
+          $what = 'geek';
+          $level = 10;
+          echo 'Hi my name is ' . $name . ' and I am level ' . $level . ' ' .
+                  $what;
+          
+          $hoursworked = $_GET['hours'];
+          //$hoursworked = 10;
+          $rate = 12;
+          
+          if ($hoursworked > 40) {
+            $total = $hoursworked * $rate * 1.5;
+          } else {
+            $total = $hoursworked * $rate;
+          }
+          echo '<br/>';
+          echo ($total > 0) ? 'You owe me ' . $total : "You're welcome";
         ?>
     </body>
 </html>
