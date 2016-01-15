ee29a37626a939101ab10e9dab611698f8ab9015
Joseph Tam <josephtam3@hotmail.com>
2016-01-14 20:58:49 -0800

    initial tictactoe logic

--- index.php
+++ index.php
@@ -6,23 +6,47 @@
     </head>
     <body>
         <?php
-          $name = 'Jim';
-          $what = 'geek';
-          $level = 10;
-          echo 'Hi my name is ' . $name . ' and I am level ' . $level . ' ' .
-                  $what;
-          
-          $hoursworked = $_GET['hours'];
-          //$hoursworked = 10;
-          $rate = 12;
-          
-          if ($hoursworked > 40) {
-            $total = $hoursworked * $rate * 1.5;
+        
+          if(!isset($_GET['board'])) {
+            echo 'No board parameter given';
           } else {
-            $total = $hoursworked * $rate;
+            $squares = $_GET['board'];
+            if(winner('x', $squares)) echo 'You win.';
+            else if(winner('o', $squares)) echo 'I win.';
+            else echo 'No winner yet.';
           }
-          echo '<br/>';
-          echo ($total > 0) ? 'You owe me ' . $total : "You're welcome";
         ?>
     </body>
 </html>
+
+<?php
+  function winner($token, $position) {
+    $won = false;
+    for($row = 0; $row < 3; $row++) {
+      if ($position[3 * $row] == $token && 
+          $position[3 * $row + 1] == $token &&
+          $position[3 * $row + 2] == $token) {
+        return true;
+      }
+    }
+    
+    for($col = 0; $col < 3; $col++) {
+      if ($position[$col] == $token && 
+          $position[$col + 3] == $token &&
+          $position[$col + 6] == $token) {
+        return true;
+      }
+    }
+    
+    if ($position[0] == $token &&
+        $position[4] == $token &&
+        $position[8] == $token) {
+      return true;
+    } else if ($position[2] == $token &&
+               $position[4] == $token &&
+               $position[6] == $token) {
+      return true;
+    }
+    return $won;
+  }
+?>
