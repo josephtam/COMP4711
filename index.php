018f0cb959f38f4126613b7962818990c74dfbb5
Joseph Tam <josephtam3@hotmail.com>
2016-01-15 00:52:39 -0800

    ai blocks player from winning otherwise takes the next available spot

--- index.php
+++ index.php
@@ -3,23 +3,36 @@
 
 
 <html>
-    <head>
-        <meta http-equiv="Content-Type" context="text/html; charset=UTF-8"> 
-        <title></title>
-    </head>
-    <body>
-        <?php
-          if(!isset($_GET['board'])) {
-            echo 'No board parameter given';
-          } else {
-            $squares = $_GET['board'];
-            $game = new Game($squares);
-            if($game->winner('x')) echo 'You win. Lucky guesses!';
-            else if($game->winner('o')) echo 'I win. Muahahahaha';
-            else echo 'No winner yet, but you are losing.';
-          }
-        ?>
-    </body>
+  <head>
+    <meta http-equiv="Content-Type" context="text/html; charset=UTF-8"> 
+    <title>Tic-Tac-Toe</title>
+  </head>
+  <body>
+    <h1>Tic-Tac-Toe Game</h1>
+    <?php
+      if(!isset($_GET['board'])) {
+       $squares = '---------';
+      } else {
+        $squares = $_GET['board'];
+      }
+      $game = new Game($squares);
+      if($squares == '---------') {
+        $game->display();
+      } else {
+        if($game->pick_move() == -1) {
+          echo 'A Tie!';
+        } else if($game->winner('x')) {
+          echo 'You win!';
+        } else if($game->winner('o')) {
+          echo 'I win!';
+        } else {
+          $game->display();
+        }
+      }  
+    ?>
+    <br />
+    <a href="/COMP4711">Restart Game</a>
+  </body>
 </html>
 
 <?php
@@ -27,6 +40,102 @@
     var $position;
     function __construct($squares) {
       $this->position = str_split($squares);
+    }
+    
+    function display() {
+      echo '<table cols="3" style="font-size:large; font-weight:bold; width:25%;">';
+      echo '<tr>';
+      for($pos = 0; $pos < 9; $pos++) {
+        echo $this->show_cell($pos);
+        if($pos % 3 == 2) echo '</tr><tr>';
+      }
+      echo '</tr>';
+      echo '</table>';
+    }
+    
+    function show_cell($which) {
+      $token = $this->position[$which];
+      //deal with the easy case
+      if($token <> '-') return '<td>' . $token . '</td>';
+      //now the hard one
+      $this->newposition = $this->position; // copy the original
+      $this->newposition[$which] = 'x'; // this would be their move
+      $move = implode($this->newposition); // make a string from the board array
+      $link = '/COMP4711?board=' . $move; // this is what we want the link to be
+      // so return a cell containing an anchor and showing a hyphen
+      return '<td><a href="' . $link . '">-</a></td>';
+    }
+    
+    function can_win($pos1, $pos2, $pos3) {
+      $xCount = 0;
+      $mtCount = 0;
+      $mtPos = -1;
+      
+      if($this->position[$pos1] == 'o' ||
+         $this->position[$pos2] == 'o' ||
+         $this->position[$pos3] == 'o') {
+        return -1;
+      } 
+      
+      if($this->position[$pos1] == '-') {
+        if($mtPos != -1) {
+          return -1;
+        } else {
+          $mtPos = $pos1;
+        }
+      }
+      
+      if($this->position[$pos2] == '-') {
+        if($mtPos != -1) {
+          return -1;
+        } else {
+          $mtPos = $pos2;
+        }
+      }
+      
+      if($this->position[$pos3] == '-') {
+        if($mtPos != -1) {
+          return -1;
+        } else {
+          $mtPos = $pos3;
+        }
+      }
+
+      return $mtPos;
+    }
+    
+    function pick_move() {
+      for($row = 0; $row < 3; $row++) {
+        if(($pos = $this->can_win(3 * $row, 3 * $row + 1, 3 * $row + 2)) != -1) {
+          $this->position[$pos] = 'o';
+          return;
+        }
+      }
+      
+      for($col = 0; $col < 3; $col++) {
+        if(($pos = $this->can_win($col, $col + 3, $col + 6)) != -1) {
+          $this->position[$pos] = 'o';
+          return;
+        }
+      }
+      
+      if(($pos = $this->can_win(0, 4, 8)) != -1) {
+        $this->position[$pos] = 'o';
+        return;
+      }
+      
+      if(($pos = $this->can_win(2, 4, 6)) != -1) {
+        $this->position[$pos] = 'o';
+        return;
+      }
+      
+      for($pos = 0; $pos < 9; $pos++) {
+        if($this->position[$pos] == '-') {
+          $this->position[$pos] = 'o';
+          return;
+        }
+      }
+      return -1;
     }
     
     function winner($token) {
@@ -47,13 +156,15 @@
         }
       }
     
-     if ($this->position[0] == $token &&
-         $this->position[4] == $token &&
-         $this->position[8] == $token) {
+      if ($this->position[0] == $token &&
+          $this->position[4] == $token &&
+          $this->position[8] == $token) {
         return true;
-      } else if ($this->position[2] == $token &&
-                 $this->position[4] == $token &&
-                 $this->position[6] == $token) {
+      }
+      
+      if ($this->position[2] == $token &&
+          $this->position[4] == $token &&
+          $this->position[6] == $token) {
         return true;
       }
       return $won;
